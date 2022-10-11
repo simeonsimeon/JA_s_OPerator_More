232ac906b1742ca1cca904c867cefd0dcd1074d6
simeonsimeon <lingakumardae@gmail.com>
2022-10-11 16:47:31 -0600

    add

---
+++ script.js
@@ -0,0 +1,45 @@
+// <!--DataTypes
+// String  Number BOOlean UNdefined NAN null
+    let string="Mango";
+    let Number=10;
+    let BOOlean = true/false;
+    var a; {undefined}
+    var b;
+    let Nan=a/b;{NaN}
+    let Null =document.getElementById("Null");
+
+    // Operator
+    var a=11;
+    var b=10;
+    assign=b;{assign}
+    a+b;{"add"}
+    a+=b;{"Assing & Add"}
+    a++;a--;{"postfix Increment and Decrement Arithmetic"}
+    ++a;--a;{"Prefix  Increment and Decrement"}
+    a==b?"true":"false";{"ternary"}
+    a===b;{'Strictly Equal {check value and datatype ......COMPARISOn}'}
+    a==b;{' Equal {check value}'}{"!=,!==,>,<,<=,>="}
+   { (a=10)&&(b=10)}("And check two condition is true")
+    {(a=10)||(b=10)};{"And check one condition is true"}
+   { (a=10)}{("!")}{(b=10)}("And check one condition is true .......LOGICaL")
+   a<<1;{"Multiple 2    left shift"}
+   a>>1;{"divide 2    right shift"}
+   {a & b}{"Bitwise And"}
+   {a | b}{"Bitwise OR"}
+   {a ^ b}{"this is Any one true is true XOR"}
+   {~a}{"Bitwise Not"}
+
+   let obj = {};
+   let obj1= []
+   console.log(obj1 instanceof Object);{"Relational then in"}
+   delete obj.a{"Unary"}
+   typeof(a)
+   var jk={a:"b",b:"sakthi"};
+   delete jk.a;
+   for(let key in jk){
+    console.log(key);
+   }
+
+
+
+    
